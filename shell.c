74d72795b852b921519e3db7df401f057520d4d9
Xilus333 <genis22@yandex.ru>
2013-10-12 16:07:08 +0400

    Command execution, environmental variables

--- shell.c
+++ shell.c
@@ -2,10 +2,14 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 #define STR_SIZE 128
 #define PARAM_COUNT 4
 #define INPUT_CH '>'
+#define BUF_SIZE 1024
 
 typedef enum { RET_OK, RET_ERR, RET_EOFOK, RET_EOFERR, RET_MEMORYERR } ret_result_t;
 
@@ -43,7 +47,7 @@ int checkParamCnt (char ***params, int nparams)
 
 int endString (char **params, int nparams, int len)
 {
-	char * ptr;
+	char *ptr;
 
 	if (len % STR_SIZE != 0)
 	{
@@ -75,7 +79,7 @@ int addChar (char **str, int *len, char ch)
 int addParam (char ***params, int *nparams)
 {	
 	if (*nparams)
-		if (checkParamCnt (params, *nparams))
+		if (checkParamCnt (params, *nparams + 1))
 			return 1;
 	if (((*params)[*nparams] = calloc (STR_SIZE, sizeof (char))))
 	{
@@ -247,12 +251,139 @@ void clearStrings (char ***params, int nparams)
 	*params = NULL;
 }
 
-void executeCommand (char **params, int nparams)
+int placeEnv (char **params, int nparams)
 {
 	int i;
-
 	for (i = 0; i < nparams; i++)
-		puts (params[i]);
+	{
+		int len = strlen (params[i]), posp = 0, poss = 0;
+		char *s, ch;
+
+		if (len < 2 || !strchr (params[i], '$'))
+			continue;
+		
+		if ((s = calloc (STR_SIZE, sizeof (char))) == NULL)
+			return 1;
+
+		while (posp < len)
+		{
+			char *env;
+			int start;
+
+			while (posp < len && params[i][posp] != '$')
+				if (checkStringLen (&s, poss))
+					return 1;
+				else
+					s[poss++] = params[i][posp++];
+			if (posp == len)
+				break;
+			start = posp + 1;
+
+			while (++posp < len && isalpha (params[i][posp]));
+
+			ch = params[i][posp];
+			params[i][posp] = '\0';
+			if ((env = getenv (params[i] + start)) != NULL)
+			{
+				if ((poss + strlen (env) + 1) / STR_SIZE > poss / STR_SIZE)
+				{
+					char *ptr;
+					if ((ptr = realloc (s, ((poss + strlen (env) + 1) / STR_SIZE + 1) * STR_SIZE * sizeof (char))) == NULL)
+						return 1;
+					s = ptr;
+				}
+				strcpy (s + poss, env);
+				poss += strlen (env);
+			}
+			params[i][posp] = ch;
+		}
+
+		if (checkStringLen (&s, poss))
+			return 1;
+		s[poss] = '\0';
+		free (params[i]);
+		params[i] = s;
+	}
+	return 0;
+}
+
+int executeCommand (char **params, int nparams)
+{
+	pid_t pid;
+
+	if (!nparams || params[0][0] == '\0')
+		return 0;
+	
+	if (placeEnv (params, nparams))
+		return 0;
+
+	if (!strcmp (params[0], "pwd"))
+	{
+		char *s = getcwd (NULL, 0);
+		if (s == NULL)
+		{
+			perror ("Error getting current directory ");
+			return 0;
+		}
+		puts (s);
+		free (s);
+		return 0;
+	}
+	if (!strcmp (params[0], "cd"))
+	{
+		if (nparams == 1)
+		{
+			if (chdir (getenv ("HOME")))
+				perror ("Error changing directory ");
+		}
+		else 
+			if (chdir (params[1]))
+				perror ("Error changing directory ");
+		return 0;
+	}
+	if (!strcmp (params[0], "exit"))
+		return 1;
+
+	if ((pid = fork ()) < 0)
+		perror ("Error creating child process ");
+	else if (pid)
+		wait (NULL);
+	else
+	{
+		params[nparams] = NULL;
+		execvp (params[0], params);
+		perror ("Error executing command ");
+		exit (0);
+	}
+
+	return 0;
+}
+
+int setEnvVars ()
+{
+	char buf[BUF_SIZE];
+	int len;
+
+	if ((len = readlink ("/proc/self/exe", buf, BUF_SIZE - 1)) == -1)
+	{
+		perror ("Error getting program name ");
+		return 1;
+	}
+	buf[len] = '\0';
+	if (setenv ("SHELL", buf, 1) == -1)
+	{
+		perror ("Error setting program name ");
+		return 1;
+	}
+
+	sprintf (buf, "%d", geteuid ());
+	if (setenv ("PUID", buf, 1) == -1)
+	{
+		perror ("Error setting puid ");
+		return 1;
+	}
+
+	return 0;
 }
 
 int main ()
@@ -261,6 +392,10 @@ int main ()
 	char **params = NULL;
 	ret_result_t ret;
 
+	signal (SIGINT, SIG_IGN);
+	if (setEnvVars ())
+		return 1;
+
 	putchar (INPUT_CH);
 
 	while ((ret = readCommand (&params, &nparams)) < RET_EOFOK)
@@ -268,7 +403,8 @@ int main ()
 		if (ret == RET_ERR)
 			puts ("Wrong format!");
 		else
-			executeCommand (params, nparams);
+			if (executeCommand (params, nparams))
+				break;
 
 		putchar (INPUT_CH);
 		clearStrings (&params, nparams);
@@ -281,7 +417,7 @@ int main ()
 		putchar ('\n');
 		puts ("Memory allocation error!");
 	}
-	else
+	else if (ret == RET_EOFERR)
 	{
 		putchar ('\n');
 		puts ("Wrong format!");
@@ -289,4 +425,4 @@ int main ()
 	clearStrings (&params, nparams);
 
 	return 0;
-} 
+}
